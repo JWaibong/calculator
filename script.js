4e32eebaefe01da2537443e62ff8602de4537f0c
Jonathan Ng <jonathan.ng@stonybrook.edu>
2021-06-08 04:58:29 -0400

    clicking number keys works correctly now

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let screenText = document.querySelector("#screenText");
-let nums = document.querySelectorAll(".number");
+let nums = document.querySelectorAll(".numberKeyText");
 let operators = document.querySelectorAll(".operator");
 
 let plus = document.querySelector("#add");
@@ -8,7 +8,23 @@ let times = document.querySelector("#multiply");
 let slash = document.querySelector("#divide");
 
 
+
 let inputs = [];
+
+nums.forEach(num => {
+    num.addEventListener("click", e => {
+        clearBeginningZero();
+        screenText.textContent += e.target.textContent.trim();
+    });
+});
+let dot = document.querySelector("#decimal");
+dot.addEventListener("click", e => {
+    if(screenText.textContent.match(/\.+/)){
+        return;
+    }
+    addBeginningZero();
+    screenText.textContent += e.target.textContent.trim();
+});
 
 plus.addEventListener("click", () => {
     plus.classList.toggle("highlighted");
@@ -48,6 +64,37 @@ slash.addEventListener("click", () => {
     }
 });
 
+
+
+
+let ac = document.querySelector("#ac");
+ac.addEventListener("click", () => {
+    clearScreen();
+    clearAllOperators();
+    inputs = [];
+});
+
+let plusMinus = document.querySelector("#plusMinus");
+plusMinus.addEventListener("click", e => {
+    screenText.textContent = `${Number(screenText.textContent) * -1}`;
+})
+
+let percent = document.querySelector("#percent");
+percent.addEventListener("click", e => {
+    screenText.textContent = `${Number(screenText.textContent) / 100.0}`;
+})
+
+function addBeginningZero(){
+    if(screenText.textContent == ""){
+        screenText.textContent = "0";
+    }
+}
+function clearBeginningZero(){
+    if(screenText.textContent.match(/^0/) && !screenText.textContent.match(/^0\./)){
+        screenText.textContent = "";
+    }
+}
+
 function isMultipleOperatorsActive(){
     return (document.querySelectorAll(".highlighted").length >= 2);
 }
@@ -57,6 +104,7 @@ function clearAllOperators(){
     });
     inputs.pop();
 }
+
 function clearScreen(){
-    screenText.textContent = "";
+    screenText.textContent = "0";
 }
