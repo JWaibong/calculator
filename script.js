6bbef1a86a5774bab739c8574da69e73d7645942
Jonathan Ng <jonathan.ng@stonybrook.edu>
2021-06-08 05:37:28 -0400

    operations work using the equals key, need to work on performing operations using the operations keys

--- script.js
+++ script.js
@@ -10,11 +10,17 @@ let slash = document.querySelector("#divide");
 
 
 let inputs = [];
-
+let operator = [];
 nums.forEach(num => {
     num.addEventListener("click", e => {
         clearBeginningZero();
-        screenText.textContent += e.target.textContent.trim();
+        if(currentOperator.match(/[+-\/\*]/)){
+            screenText.textContent = e.target.textContent.trim();
+            currentOperator = "";
+        }
+        else{
+            screenText.textContent += e.target.textContent.trim();
+        }
     });
 });
 let dot = document.querySelector("#decimal");
@@ -26,12 +32,16 @@ dot.addEventListener("click", e => {
     screenText.textContent += e.target.textContent.trim();
 });
 
+
+let currentOperator = ""
 plus.addEventListener("click", () => {
     plus.classList.toggle("highlighted");
     if(isMultipleOperatorsActive()){
         clearAllOperators();
     }
     else{
+        currentOperator = "+";
+        operator.push("+");
         inputs.push(screenText.textContent.substring(0, 9));
     }
 });
@@ -41,6 +51,8 @@ minus.addEventListener("click", () => {
         clearAllOperators();
     }
     else{
+        currentOperator = "-";
+        operator.push("-");
         inputs.push(screenText.textContent.substring(0, 9));
     }
 });
@@ -50,16 +62,19 @@ times.addEventListener("click", () => {
         clearAllOperators();
     }
     else{
+        currentOperator = "\*";
+        operator.push("\*");
         inputs.push(screenText.textContent.substring(0, 9));
     }
 });
-
 slash.addEventListener("click", () => {
     slash.classList.toggle("highlighted");
     if(isMultipleOperatorsActive()){
         clearAllOperators();
     }
     else{
+        currentOperator = "\/";
+        operator.push("\/");
         inputs.push(screenText.textContent.substring(0, 9));
     }
 });
@@ -84,6 +99,40 @@ percent.addEventListener("click", e => {
     screenText.textContent = `${Number(screenText.textContent) / 100.0}`;
 })
 
+let equals = document.querySelector("#equals");
+equals.addEventListener("click", e => {
+    clearAllOperatorsWithoutClearingInput();
+    if(screenText.textContent.match(/\d+/) && operator[0]!==null){
+        switch(operator[0]){
+            case "+":
+                screenText.textContent = `${Number(screenText.textContent) + Number(inputs[0])}`;
+                inputs.pop();
+                operator.pop();
+                break;
+            case "-":
+                screenText.textContent = `${Number(inputs[0]) - Number(screenText.textContent)}`;
+                inputs.pop();
+                operator.pop();
+                break;
+            case "\*":
+                screenText.textContent = `${Number(screenText.textContent) * Number(inputs[0])}`;
+                inputs.pop();
+                operator.pop();
+                break;
+            case "\/":
+                screenText.textContent = `${Number(inputs[0]) / Number(screenText.textContent) }`;
+                inputs.pop();
+                operator.pop();
+                break;
+            default:
+                console.log("error");
+        }
+    }
+
+
+});
+
+
 function addBeginningZero(){
     if(screenText.textContent == ""){
         screenText.textContent = "0";
@@ -99,12 +148,18 @@ function isMultipleOperatorsActive(){
     return (document.querySelectorAll(".highlighted").length >= 2);
 }
 function clearAllOperators(){
-    document.querySelectorAll(".highlighted").forEach(operator => {
-        operator.classList.toggle("highlighted");
+    document.querySelectorAll(".highlighted").forEach(op => {
+        op.classList.toggle("highlighted");
     });
+    currentOperator = "";
     inputs.pop();
 }
-
+function clearAllOperatorsWithoutClearingInput(){
+    document.querySelectorAll(".highlighted").forEach(op => {
+        op.classList.toggle("highlighted");
+    });
+    currentOperator = "";
+}
 function clearScreen(){
     screenText.textContent = "0";
 }
